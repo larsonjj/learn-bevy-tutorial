ab7472d728d9c9bb56c0d0cda3b462e751f8019d
Jake Larson <jake.j.larson@gmail.com>
2023-04-02 11:33:54 -0500

    Replaced kira audio with the built in one

--- src/audio.rs
+++ src/audio.rs
@@ -2,33 +2,22 @@ use crate::enemy::EnemyDirectionChangedEvent;
 use crate::loading::AudioAssets;
 use crate::GameState;
 use bevy::prelude::*;
-use bevy_kira_audio::prelude::*;
 
 pub struct InternalAudioPlugin;
 
 // This plugin is responsible to control the game audiocargo
 impl Plugin for InternalAudioPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(AudioPlugin)
-            .add_system(start_audio.in_schedule(OnEnter(GameState::Playing)))
-            .add_system(play_background_music.in_set(OnUpdate(GameState::Playing)))
+        app.add_system(start_audio.in_schedule(OnEnter(GameState::Playing)))
             .add_system(control_enemy_direction_changed_sound.in_set(OnUpdate(GameState::Playing)));
     }
 }
 
 #[derive(Resource)]
-struct BackgroundMusicAudio(Handle<AudioInstance>);
+struct BackgroundMusicAudio(Handle<AudioSource>);
 
-fn start_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
-    audio.pause();
-    let background_music_handle = audio
-        .play(audio_assets.background_music.clone())
-        .looped()
-        .with_volume(0.3)
-        .handle();
-
-    // Insert handles into the world so that we can control the audio later
-    commands.insert_resource(BackgroundMusicAudio(background_music_handle));
+fn start_audio(audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
+    audio.play(audio_assets.background_music.clone());
 }
 
 fn control_enemy_direction_changed_sound(
@@ -40,25 +29,6 @@ fn control_enemy_direction_changed_sound(
         println!("Enemy direction changed event received");
         // This prevents events staying active on the next frame.
         enemy_direction_changed_events.clear();
-        audio
-            .play(audio_assets.enemy_direction_changed_1.clone())
-            .with_volume(0.3);
-    }
-}
-
-fn play_background_music(
-    audio: Res<BackgroundMusicAudio>,
-    mut audio_instances: ResMut<Assets<AudioInstance>>,
-) {
-    if let Some(instance) = audio_instances.get_mut(&audio.0) {
-        match instance.state() {
-            PlaybackState::Paused { .. } => {
-                instance.resume(AudioTween::default());
-            }
-            PlaybackState::Playing { .. } => {
-                instance.pause(AudioTween::default());
-            }
-            _ => {}
-        }
+        audio.play(audio_assets.enemy_direction_changed_1.clone());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
 use crate::player::PlayerPlugin;
 
-use bevy::app::App;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
@@ -38,11 +37,11 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_state::<GameState>()
-            .add_plugin(InternalAudioPlugin)
             .add_plugin(CameraPlugin)
             .add_plugin(LoadingPlugin)
             .add_plugin(MenuPlugin)
             .add_plugin(ActionsPlugin)
+            .add_plugin(InternalAudioPlugin)
             .add_plugin(PlayerPlugin)
             .add_plugin(EnemyPlugin);
 
