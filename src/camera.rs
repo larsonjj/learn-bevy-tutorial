665296c64075bd13857516cc82ee6f2d5cba6dc3
Jake Larson <jake.j.larson@gmail.com>
2023-04-01 23:39:06 -0500

    Added player contraints to bounds of camera. Extracted Camera into it's own plugin

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 pub struct CameraPlugin;
 
@@ -9,6 +10,11 @@ impl Plugin for CameraPlugin {
     }
 }
 
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+fn setup_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1000.0),
+        ..default()
+    });
 }
--- src/player.rs
+++ src/player.rs
@@ -2,26 +2,37 @@ use crate::actions::Actions;
 use crate::loading::TextureAssets;
 use crate::GameState;
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 pub struct PlayerPlugin;
 
 #[derive(Component)]
 pub struct Player;
 
+const PLAYER_SPEED: f32 = 150.;
+const PLAYER_SIZE: f32 = 64.;
+
 /// This plugin handles player related stuff like movement
 /// Player logic is only active during the State `GameState::Playing`
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(spawn_player.in_schedule(OnEnter(GameState::Playing)))
-            .add_system(move_player.in_set(OnUpdate(GameState::Playing)));
+            .add_system(move_player.in_set(OnUpdate(GameState::Playing)))
+            .add_system(confine_player_to_window.in_set(OnUpdate(GameState::Playing)));
     }
 }
 
-fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
+fn spawn_player(
+    mut commands: Commands,
+    textures: Res<TextureAssets>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap();
+
     commands
         .spawn(SpriteBundle {
             texture: textures.texture_ball.clone(),
-            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
             ..default()
         })
         .insert(Player);
@@ -36,7 +47,7 @@ fn move_player(
         return;
     }
 
-    let speed = 150.;
+    let speed = PLAYER_SPEED;
     let movement = Vec3::new(
         actions.player_movement.unwrap().x * speed * time.delta_seconds(),
         actions.player_movement.unwrap().y * speed * time.delta_seconds(),
@@ -44,5 +55,36 @@ fn move_player(
     );
     for mut player_transform in &mut player_query {
         player_transform.translation += movement;
+    }
+}
+
+// function to confine player movement to the window
+fn confine_player_to_window(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+        let half_player_size = PLAYER_SIZE / 2.;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player.translation = translation;
     }
 }
